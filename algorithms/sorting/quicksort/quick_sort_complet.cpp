ed1167c8c516d1adaf5570eb4fc576518a99c381
Adrian Statescu <mergesortv@gmail.com>
2015-01-11 13:33:28 -0800

    quick sort

---
+++ algorithms/sorting/quicksort/quick_sort_complet.cpp
@@ -0,0 +1,135 @@
+/**
+ *  Quick Sort within Container
+ *  Compile -> g++ std=c++0x quicksort.cpp -o quick
+ */
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#define FIN "algsort.in"
+#define FOUT "algsort.out"
+#define push push_back
+#define MAXCHAR 5500000
+ 
+typedef unsigned int uint;
+
+char str[ MAXCHAR ];
+ 
+class Container {
+ 
+      public:
+      Container( uint _N )
+                         :
+                         N(_N)
+      {
+      };
+ 
+      ~Container(){};
+ 
+      void add_to_container(uint elem)
+      {
+           vec.push( elem );    
+      }
+ 
+      void sorted()
+      {
+ 
+           _qsort(0, N - 1);
+      };
+ 
+      friend std::ostream &operator << (std::ostream &stream, const Container &obj)
+      {
+ 
+             for(auto const el: obj.vec) {
+ 
+                 stream<<el<<" "; 
+             }
+  
+             return stream;
+      };
+ 
+      private:
+      std::vector<uint> vec;
+      uint N;
+ 
+      void _s(int x, int y)
+      {
+ 
+           int aux;
+ 
+               aux = vec[ x ] ^ vec[ y ];
+ 
+               vec[ x ] = aux ^ vec[ x ];
+ 
+               vec[ y ] = aux ^ vec[ y ];
+      };
+ 
+      void _qsort(int low, int high)
+      {
+ 
+           int i = low,
+ 
+               j = high,
+ 
+               piv = vec[ (low + high) >> 1 ];
+ 
+               while(i <= j) {
+ 
+                     while(vec[ i ] < piv) i++;
+ 
+                     while(vec[ j ] > piv) j--;
+ 
+                     if( i <= j ) {_s(i, j); i++; j--;}
+               }
+ 
+               if(low < j) _qsort(low, j);
+ 
+               if(i < high) _qsort(i, high);
+      };
+ 
+};
+ 
+int main() {
+ 
+    uint N;
+ 
+    freopen(FIN, "r", stdin);
+ 
+    scanf("%d\n",&N);
+ 
+    Container obj( N );
+ 
+    gets( str );
+ 
+    fclose( stdin );
+ 
+    str[strlen(str)] = ' ';
+ 
+    int j = 0;
+ 
+    for(int i = 0; i < N; i++) {
+ 
+        int num = 0;
+          
+        while(str[ j ] != ' ') {
+ 
+              num = num * 10 + str[ j ] - '0';
+ 
+              j++;
+        }
+ 
+        obj.add_to_container( num );
+ 
+        j++;
+    } 
+ 
+    obj.sorted();
+ 
+    std::ofstream fout( FOUT );
+ 
+    fout<<obj;
+ 
+    fout.close();
+  
+  return(0);
+};
