302547d8eda287a26f0aa1d7e91ea6f37e8a0ac5
Adrian Statescu <mergesortv@gmail.com>
2015-11-15 07:49:02 -0800

    stuff

---
+++ algorithms/sorting/mergesort/m4.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <malloc.h>
+#define FIN "algsort.in"
+#define FOUT "algsort.out"
+#define MAXN 500005
+
+int *v, 
+    n, 
+    *aux;
+
+void read() {
+
+     int i; 
+
+     FILE *fin = fopen(FIN, "r");
+
+     fscanf(fin, "%d", &n);     
+
+     v = (int*)malloc(sizeof(int) * n);
+
+     for(i = 0; i < n; ++i) fscanf(fin, "%d", v + i);
+}
+
+void divideEtImpera(int lo, int hi) {
+    
+     if(lo == hi) return;
+
+     int m = (lo + hi) >> 1;
+
+     divideEtImpera(lo, m);
+
+     divideEtImpera(m+1, hi);
+     
+     int i = lo, 
+         j = m + 1, 
+         k = lo, 
+         it;
+
+     aux = ( int* ) malloc( sizeof(int) * (n) );
+
+     for(it = lo; it <= m; ++it) aux[ it ] = v[ it ];
+
+     while(i <= m && j <= hi ) {
+
+           if(aux[ i ] < v[ j ]) v[k++] = aux[ i++ ]; 
+
+                      else     v[k++] = v[ j++ ]; 
+     }
+
+     while(i<=m) v[k++] = aux[ i++ ]; 
+
+     while(j<=hi) v[k++] = v[ j++ ];       
+
+     free(aux); 
+}
+
+void write() {
+
+     int i;
+
+     FILE *fout = fopen(FOUT, "w");
+
+     for(i = 0; i < n; ++i) fprintf(fout, "%d ", *(v + i));
+}
+
+int main() {
+
+    read();
+    divideEtImpera(0, n - 1);
+    write();
+
+    return(0);  
+}
