e5cc975ec0a4f7a68afa1a964d326b9d1dc21ded
Adrian Statescu <mergesortv@gmail.com>
2015-11-16 20:13:10 -0800

    mergesort with pointers new and malloc

---
+++ algorithms/sorting/mergesort/m_8.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <malloc.h>
+#define FIN "algsort.in"
+#define FOUT "algsort.out"
+#define MAXN 500005
+
+int *v, 
+    n;
+
+void read() {
+
+     int i; 
+
+     FILE *fin = fopen(FIN, "r");
+
+     fscanf(fin, "%d", &n);     
+
+     v = (int*)malloc(sizeof(int)*n);
+
+     for(i = 0; i < n; ++i) fscanf(fin, "%d", v + i);
+}
+
+void _merge(int lo, int m, int hi) {
+
+     int i = lo, 
+
+         j = m + 1,
+
+         k = lo,
+
+         it, 
+
+         *aux;
+
+     aux = ( int* )malloc(sizeof(int)* ( hi - lo + 1 ) ); 
+
+     for(it = lo; it <= hi; ++it) aux[ it - lo ] = v[ it ];
+
+     while(i <= m && j <= hi ) {
+
+           if(aux[ i - lo ] < aux[ j - lo ]) v[k++] = aux[ i++ - lo]; 
+
+                      else  
+                                             v[k++] = aux[ j++ - lo]; 
+     }
+
+     while(i<=m) v[k++] = aux[ i++ - lo]; 
+
+     while(j<=hi) v[k++] = aux[ j++ - lo];
+
+     free( aux );
+}
+
+
+void divideEtImpera(int lo, int hi) {
+    
+     if(lo == hi) return;
+
+     int m = (lo + hi) >> 1;
+
+     divideEtImpera(lo, m);
+
+     divideEtImpera(m+1, hi);
+
+     _merge(lo, m, hi);     
+}
+
+void write() {
+
+     int i;
+
+     FILE *fout = fopen(FOUT, "w");
+
+     for(i = 0; i < n; ++i) fprintf(fout,"%d ", *(v + i));
+}
+
+int main() {
+
+    read();
+    divideEtImpera(0, n - 1);
+    write();
+
+    return(0);  
+}
---
+++ algorithms/sorting/mergesort/m_9.cpp
@@ -0,0 +1,85 @@
+#include <istream>
+#include <fstream>
+#define FIN "algsort.in"
+#define FOUT "algsort.out"
+#define MAXN 500005
+
+int *v, 
+    n;
+
+using namespace std;
+
+void read() {
+
+     int i; 
+
+     ifstream fin(FIN);
+
+     fin>>n;
+
+     v = new int[ n ];
+
+     for(i = 0; i < n; ++i) fin>>v[i];
+
+     fin.close();
+}
+
+void _merge(int lo, int m, int hi) {
+
+     int i = lo, 
+         j = m + 1,
+         k = lo,
+         it, 
+         *aux;
+
+     aux = new int[hi-lo+1];
+
+     for(it = lo; it <= hi; ++it) aux[ it - lo ] = v[ it ];
+
+     while(i <= m && j <= hi ) {
+
+           if(aux[ i - lo ] < aux[ j - lo ]) v[k++] = aux[ i++ - lo]; 
+
+                      else  
+                                             v[k++] = aux[ j++ - lo]; 
+     }
+
+     while(i<=m) v[k++] = aux[ i++ - lo]; 
+
+     while(j<=hi) v[k++] = aux[ j++ - lo];
+
+     delete [] aux;
+};
+
+void divideEtImpera(int lo, int hi) {
+    
+     if(lo == hi) return;
+
+     int m = (lo + hi) >> 1;
+
+     divideEtImpera(lo, m);
+
+     divideEtImpera(m+1, hi);
+
+     _merge(lo, m, hi);     
+}
+
+void write() {
+
+     int i;
+
+     ofstream fout(FOUT);
+
+     for(i = 0; i < n; ++i) fout<<v[i]<<" ";
+
+     fout.close();
+}
+
+int main() {
+
+    read();
+    divideEtImpera(0, n - 1);
+    write();
+
+    return(0);  
+}
