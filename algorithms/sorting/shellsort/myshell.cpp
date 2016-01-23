cab2d3f47ac6d43f72943de87f265206f762b127
Adrian Statescu <mergesortv@gmail.com>
2016-01-23 11:14:35 -0800

    sorting shell 2016

---
+++ algorithms/sorting/shellsort/myshell.cpp
@@ -0,0 +1,117 @@
+/**
+ * ShellSort in action.
+ */
+#include <iostream>
+#include <fstream>
+#include <vector>
+#define push push_back
+#define FIN "algsort.in"
+#define FOUT "algsort.out"
+
+using namespace std;
+
+class Container {
+
+      public:
+      Container(int _n): n( _n ){};
+
+      ~Container(){}; 
+
+      void added(vector<int> v) {
+
+           vec = v;       
+      };
+
+      void sort() {
+
+           _shellsort();  
+      };
+
+      friend ostream &operator << (ostream &stream, const Container &ob ) {
+
+             vector<int> vec = ob.vec; 
+
+             for(vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)                
+            
+                 stream<<*it<<" ";
+
+             return stream;
+      }; 
+
+      private:
+      vector<int> vec;
+      int n;
+
+      void _shellsort() {
+
+           int gap = n - 1,
+
+               g,
+
+               i,
+
+               j, temp;
+
+               for(g = gap/2; g > 0; g /= 2 ) {
+
+                   for(i = g; i < n; i++) {
+
+                       temp = vec[ i ];
+
+                       for(j = i; j >= g; j -= g) {
+
+                           if( vec[ j - g ] > temp )
+
+                               vec[ j ] = vec[ j - g ];
+
+                           else 
+
+                               break;
+                       } 
+
+                       vec[ j ] = temp;
+                   }
+               }  
+      };
+};
+
+int main() {
+
+    int n;
+
+    std::ifstream fin( FIN );
+
+    std::ofstream fout( FOUT );
+
+    int elem;
+
+    vector<int> vec;
+
+    vector<int>::iterator it;
+
+    fin>>n; 
+
+    Container cont( n );
+
+    it = vec.begin();
+
+    while( n-- ) {
+
+           fin>>elem;
+
+           it = vec.insert(it, elem);
+    } 
+
+    cont.added( vec ); 
+
+    cont.sort();
+
+    fout<<cont;
+
+    fin.close();
+
+    fout.close();
+
+    return(0);
+};
+
