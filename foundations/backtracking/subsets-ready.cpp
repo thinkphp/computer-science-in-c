7ebfd9e697011532c4224865df5e51f06f7fff13
Adrian Statescu <mergesortv@gmail.com>
2016-10-26 20:10:01 -0700

    subsets ready to go

---
+++ foundations/backtracking/subsets-ready.cpp
@@ -0,0 +1,91 @@
+#include <cstdio>
+#include <fstream>
+#include <vector>
+ 
+using namespace std;
+
+vector<int> Stack(100);
+int level,
+    N;
+ 
+void printSolution() {
+ 
+     for(int i = 1; i <= level; ++i) printf("%d ", Stack[ i ]);
+
+     printf("\n");
+}
+ 
+int solution() {
+ 
+    return 1;   
+}
+ 
+int successor() {
+ 
+    if(Stack[ level ] < N) {
+
+       Stack[ level ]++;
+
+       return 1;
+    } 
+ 
+    return 0;  
+}
+ 
+int valid() {
+ 
+    return 1; 
+}
+ 
+void init() {
+
+
+     Stack[ level ] = Stack[level-1];
+}
+ 
+void back() {
+ 
+   int HN, is;
+
+   level = 1; 
+
+   init();
+ 
+   while(level > 0) {   
+  
+     HN = 1; is = 0;
+
+     while(HN && !is){
+
+           HN = successor();
+
+           if(HN) is = valid();
+     }
+ 
+     if( HN ) {
+ 
+
+             if(solution()) {printSolution();level++;init();}
+
+
+ 
+     } else {
+ 
+       level--; 
+     }
+ 
+   }
+}
+ 
+
+ 
+int main() {
+ 
+
+  N = 3;
+ 
+  back();   
+
+ 
+ return(0);
+};
