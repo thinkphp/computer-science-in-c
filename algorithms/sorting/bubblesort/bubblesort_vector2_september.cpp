11e5b9be0c9b833af72fa2f4c9373f441a6d463a
Adrian Statescu <mergesortv@gmail.com>
2017-09-05 03:39:07 -0700

    bubblesort iterator

---
+++ algorithms/sorting/bubblesort/bubblesort_vector2_september.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#define FIN "algsort.in"
+#define FOUT "algsort.out"
+
+using namespace std;
+
+typedef vector<int>::iterator I;
+
+void sort(vector<int> arr) {
+
+     ofstream fout(FOUT);
+
+     I last = arr.end() - 1;
+
+     bool finished = false,
+          swapped;    
+ 
+     while(!finished) {
+
+            swapped = false;
+
+            for(I it = arr.begin(); it != last; ++it) 
+
+                if((*it) > *(it+1)) {
+
+                   swap(*it, *(it+1));
+
+                   swapped = true;  
+                }
+
+            if(swapped) last--;
+
+               else     finished = true;
+     }
+
+     for(auto v:arr) fout<<v<<" ";     
+};
+
+int main() {
+
+    int n;    
+
+    ifstream fin(FIN);
+
+    fin>>n;
+
+    int arr[ n ];
+
+    for(int i = 0; i < n; ++i) fin>>arr[i];
+
+    sort(vector<int>(arr, arr + sizeof(arr)/sizeof(int))); 
+
+ return(0);
+};
