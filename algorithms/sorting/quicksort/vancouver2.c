6909e3e1762bb8bba0de2df2ebe8d9a1d2d241a7
Adrian Statescu <mergesortv@gmail.com>
2018-04-02 21:09:13 -0700

    quicksort using function to pointer with 20px infoarena

---
+++ algorithms/sorting/quicksort/vancouver2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void _quicksort(int *p, int lo, int hi) {
+
+     int piv = p[ (lo + hi) >> 1], i = lo, j = hi;      
+
+     while(i <= j) {
+
+           while(p[i]<piv) i++;
+
+           while(p[j]>piv) j--;
+
+           if(i<=j) {
+
+                int x = p[i]^p[j];
+                    p[i] = p[i]^x;
+                    p[j] = p[j]^x;
+                    i++;
+                    j--;
+           } 
+
+           if(lo<j) _quicksort(p, lo, j);
+
+           if(hi>i) _quicksort(p, i, hi);
+     } 
+
+}
+
+void quicksort(int *p, int n) {
+
+     _quicksort(p, 0, n - 1);
+      
+}
+
+int main() {
+
+    const char *fin = "algsort.in";
+    const char *fout = "algsort.out";
+
+    int *v,
+         n,
+         i;   
+
+    freopen(fin,"r",stdin);
+
+    freopen(fout,"w",stdout);
+
+    scanf("%d", &n);
+
+    v = (int*)malloc(sizeof(int)  *  n);
+
+    for(i = 0; i < n; ++i) scanf("%d", v + i);
+
+    quicksort(v, n);
+
+    for(i = 0; i < n; ++i) printf("%d ", *(v + i));
+
+    free( v ); 
+
+ return(0);
+}
---
+++ algorithms/sorting/quicksort/vancouver4.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void _quicksort(int *p, int lo, int hi) {
+
+     int piv = p[ (lo + hi) >> 1], i = lo, j = hi;      
+
+     while(i <= j) {
+
+           while(p[i]<piv) i++;
+
+           while(p[j]>piv) j--;
+
+           if(i<=j) {
+
+                int x = p[i]^p[j];
+                    p[i] = p[i]^x;
+                    p[j] = p[j]^x;
+                    i++;
+                    j--;
+           } 
+
+           if(lo<j) _quicksort(p, lo, j);
+
+           if(hi>i) _quicksort(p, i, hi);
+     } 
+
+}
+
+void quicksort(int *p, int n) {
+
+     _quicksort(p, 0, n - 1);
+      
+}
+
+int main() {
+
+    const char *fin = "algsort.in";
+    const char *fout = "algsort.out";
+
+    int v[50005],
+        n,
+        i;   
+
+    freopen(fin,"r",stdin);
+
+    freopen(fout,"w",stdout);
+
+    scanf("%d", &n);
+
+
+    for(i = 0; i < n; ++i) scanf("%d", v + i);
+
+    quicksort(v, n);
+
+    for(i = 0; i < n; ++i) printf("%d ", *(v + i));
+
+
+ return(0);
+}
