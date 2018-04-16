25c7baea0130cbcc3a071aab791f0741db1a27e1
Adrian Statescu <mergesortv@gmail.com>
2018-04-16 03:08:27 -0700

    quicksort using pointer to function

---
+++ algorithms/sorting/quicksort/ironmansort.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <malloc.h>
+#define FIN "algsort.in"
+#define FOUT "algsort.out"
+
+typedef void (*PTRfn)(int *p, int);
+
+void merge(int *p, int lo, int m, int hi) {
+
+     int i = lo, 
+         j = m + 1, 
+         index = 0, 
+         aux[hi - lo + 1];
+
+     while(i <= m && j <= hi) {
+
+           if(p[i] < p[j]) aux[index++] = p[i++];
+           else  
+           aux[index++] = p[j++];
+     }
+
+     while(i<=m) aux[index++] = p[i++];
+
+     while(j<=hi) aux[index++] = p[j++];
+
+     index = 0;  
+
+     for(i = lo; i <= hi; ++i) p[i] = aux[index++]; 
+};
+
+void divideetimpera(int *p, int lo, int hi) {
+
+     if(lo < hi) {
+        int m = (lo+hi)>>1;
+        divideetimpera(p, lo, m);
+        divideetimpera(p, m + 1, hi);
+        merge(p, lo, m, hi);
+     }  
+}
+
+void mergesort(int *p, int n) {
+
+     divideetimpera(p, 0, n - 1 );
+}
+
+void _quicksort(int *p, int lo, int hi) {
+
+     int i = lo, j = hi, piv = p[(lo+hi)>>1];
+
+     while(i<=j) {
+           while(p[i]<piv) i++;
+           while(p[j]>piv) j--;
+           if(i<=j) {
+              int tmp = p[i];
+                  p[i] = p[j];
+                  p[j] = tmp;
+                  i++;j--;  
+           }
+     }  
+
+     if(i < hi) _quicksort(p, i, hi);
+     if(lo < j) _quicksort(p, lo, j); 
+}
+
+void quicksort(int *p, int n) {
+
+     _quicksort(p, 0, n - 1); 
+}
+
+int main() {
+
+    int *p, n, i;
+
+    freopen(FIN, "r", stdin);
+
+    freopen(FOUT, "w", stdout);
+
+    PTRfn s = quicksort; 
+
+    scanf("%d", &n);
+
+    p = (int*)malloc(sizeof(int) * n); 
+
+    for(i = 0; i < n; ++i) scanf("%d ", (p + i));
+
+    s(p, n); 
+
+    for(i = 0; i < n; ++i) printf("%d ", *(p + i));
+
+    free( p );
+
+ return( 0 );
+};
