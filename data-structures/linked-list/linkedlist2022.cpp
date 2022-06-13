5320952bdcd3b1b8316b12ded9b0a404427ddbcd
Adrian Statescu <mergesortv@gmail.com>
2022-06-13 17:28:42 +0300

    Update linkedlist2022.cpp

--- data-structures/linked-list/linkedlist2022.cpp
+++ data-structures/linked-list/linkedlist2022.cpp
@@ -1,3 +1,16 @@
+/*
+    Singly Linked List Data Structure
+
+ - addAtBeginning(head, key)
+ - addAtEnd(head, key)
+ - removeNode(head, key)
+ - display(head)
+ - insertAfterNode(key, value)
+ - insertBeforeNode(key, value)
+ - search(head, key)
+ - reverse(head)
+
+*/
 #include <iostream>
 
 using namespace std;
@@ -26,6 +39,17 @@ Node* add(Node *&head, int value) {
      return head;
 }
 
+int searchNode(Node*head,int key) {
+    Node *c = head;
+
+    while(c) {
+          if(c->data == key) return 1;
+          c = c->next;
+    }
+
+    return 0;
+}
+
 Node* add_at_end(Node *&head, int value) {
 
      if(head == NULL) {
@@ -50,7 +74,73 @@ void display(Node *head) {
          cout<<curr->data<<" ";
          curr = curr->next;
      }
+
+     cout<<("\n");
 }
+
+Node* insertAfterNode(Node* head,  int key, int value) {
+
+      Node *c = head;
+
+      while(c->data != key) c = c->next;
+
+      Node *node;
+      node = new Node();
+      node->data = value;
+      node->next = c->next;
+      c->next = node;
+
+      return node;
+};
+
+Node* insertBeforeNode(Node *&head, int key, int val ) {
+
+      if(head->data == key) {
+
+         Node *c = new Node();
+               c->data = val;
+               c->next = head->next;
+               head = c;
+      } else {
+        Node *ptr = head;
+        while(ptr->next->data != key) ptr = ptr->next;
+        Node *c = new Node();
+        c->data = val;
+        c->next = ptr->next;
+        ptr->next = c;
+      }
+
+      return head;
+};
+
+Node* removeNode(Node *head, int key ) {
+
+      Node *c;
+
+      //if the node to delete is even header
+      if(head->data == key)
+      {
+         c = head;
+         head = c->next;
+         delete c;
+
+      } else {
+
+        c = head;
+
+        Node *ptr;
+
+        while(c->next->data != key) c = c->next;
+
+        ptr = c->next;
+
+        c->next = ptr->next;
+
+        delete ptr;
+      }
+
+      return head;
+};
 
 int main(int argc, char const *argv[]) {
 
@@ -63,7 +153,25 @@ int main(int argc, char const *argv[]) {
     cin>>val;
   }
 
-  display(head);
+  display( head );
 
+  int del;
+  cout<<"del=";
+  cin>>del;
+
+  head = removeNode( head, del );
+
+  display( head );
+
+  int node = 9;
+  insertAfterNode(head, node, 7777);
+  display( head );
+
+  int node2 = 66;
+  insertBeforeNode(head, node2, -7777);
+  display( head );
+
+  int s = searchNode(head, 5);
+  cout<<"Search: "<<s;
   return 0;
 }
