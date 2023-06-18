5e88f17fb0932be314b029bcdfb25fd1a47623a6
Adrian Statescu <mergesortv@gmail.com>
2023-06-18 09:28:58 +0300

    Update ValidTriangleNumber.cpp

--- algorithms/searching/BinarySearch/ValidTriangleNumber.cpp
+++ algorithms/searching/BinarySearch/ValidTriangleNumber.cpp
@@ -1,5 +1,7 @@
 /*
- * Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+ * Given an integer array nums, return the number of triplets chosen from the 
+*  array that can make triangles if we take 
+ * them as side lengths of a triangle.
  */
 
 class Solution {
