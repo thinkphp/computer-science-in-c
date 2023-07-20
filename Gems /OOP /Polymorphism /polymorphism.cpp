50d869320e8b4f9b48ab8a9462c852b91c08aa7e
Adrian Statescu <mergesortv@gmail.com>
2023-07-20 18:11:30 +0300

    Create polymorphism.cpp

---
+++ Gems /OOP /Polymorphism /polymorphism.cpp
@@ -0,0 +1,57 @@
+/*
+ Object Oriented Programming OOP 
+ - Inheritance
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Base {
+
+    public: 
+      int nr;
+      int num;
+      
+      virtual void getNumarator() {
+           cout<<"Numarator = "<<nr;
+      }
+
+      virtual int getNumitor() {
+           cout<<"Numitor = "<<num;
+      }
+
+      void read() {
+           cout<<"Numarator=";
+           cin>>nr;
+           cout<<"Numitor=";
+           cin>>num; 
+      }
+
+      void display() {
+           getNumarator();
+           cout<<endl;
+           getNumitor();
+      }
+};
+
+class Derivate: public Base {
+
+      public:      
+      void getNumarator() {
+           cout<<"Apply Polymorphism. Numarator => "<<nr;
+      }
+
+      int getNumitor() {
+           cout<<"Apply Polymorphism. Numitor => "<<num;
+      }
+};
+
+int main() {
+
+    Derivate f;
+
+    f.read();
+    f.display();
+
+    return( 0 );
+};
