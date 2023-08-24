3fe5fedc89b05d70c7dd0e51c2943b2c6563b6e7
Adrian Statescu <mergesortv@gmail.com>
2023-08-24 21:07:20 +0300

    Create ptrFn.cpp

---
+++ Gems /PtrToFunctions /ptrFn.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+
+typedef struct student {
+  char name[100];
+  float cs,
+        maths,
+        phisics;
+} Student;
+
+typedef void(*fnPtr)(int*, int);
+
+void bubblesort(int *p, int size) {
+    printf("\nbubblesort method call");
+}
+
+void insertsort(int *p, int size) {
+    printf("\ninsertsort method call");
+}
+
+int main(int argc, char const *argv[]) {
+
+  Student ob,
+          *ob2,//pointer to Student
+          *ob3;
+  ob3 = new Student;
+  strcpy(ob3->name, "State");
+  ob2 = &ob;
+  strcpy(ob.name, "adrian");
+  ob.cs = 7.5;
+  ob.maths = 10;
+  ob.phisics = 8;
+  printf("%s\n", ob.name);
+  printf("%s\n", ob2->name);
+  printf("%s", ob3->name);
+
+  fnPtr f;
+  int arr[6] = {5,4,3,2,1,0}, size = 6;
+  f = insertsort;
+  f(arr, size);
+  return 0;
+}
