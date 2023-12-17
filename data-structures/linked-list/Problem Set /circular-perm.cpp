f659c61d2e891229e31d19eca5557a442ab30fd5
Adrian Statescu <mergesortv@gmail.com>
2023-12-17 06:48:55 +0200

    Create circular-perm.cpp

---
+++ data-structures/linked-list/Problem Set /circular-perm.cpp
@@ -0,0 +1,15 @@
+/*
+Book: 
+Fundamentele Programarii. Culegere de probleme pentru clasa a X1-a. Autori: Dana Lica si Mircea Pasoi
+
+Task: Realizati in program care afiseaza numerele obtinute prin permutari circulare ale
+cifrelor unui numar natural Nr preluat de la tastatura. Cifrele lui Nr vor fi
+retinute intr-o lista simplu inlantuita. Vor fi implementate doua subprograme:
+
+- subprogramul Lista, care permite memorarea cifrelor numarului Nr intr-o lista
+simplu inlantuita. Subprogramul va returna adresa de inceput a listei create prin intermediul unui parametru
+- subprogramul Permut, care primind la apel, prin intermediul unui parametru, adresa de inceput a unui liste, realizeaza transferul primului element la finalul listei, fara a folosi memorie suplimentar. Subprogramul va returna noua adresa de inceput a listei prin intermediul aceluiasi parametru.
+
+Input: Nr = 1234
+Output: 2341, 3412, 4123, 1234
+*/
