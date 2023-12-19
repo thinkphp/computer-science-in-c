f1d18263d9dd604ad147d8e71b7c2ceedf9e5be3
Adrian Statescu <mergesortv@gmail.com>
2023-12-20 00:44:04 +0200

    Update split-lists-odd-even.cpp

--- data-structures/linked-list/Problem Set /split-lists-odd-even.cpp
+++ data-structures/linked-list/Problem Set /split-lists-odd-even.cpp
@@ -35,13 +35,86 @@ Output:
 */
 #include <iostream>
 using namespace std;
+ 
 struct Node {
-     int data;
-     struct Node *next;
+    int data;
+    Node *next;
 };
-
-
+ 
+void CreateList(Node *&head) {
+    Node *q, *last;
+    int node, n;
+    head = nullptr;
+    last = nullptr;
+ 
+    cout << "number of nodes=";
+    cin >> n;
+ 
+    while (n--) {
+        cout << "node=";
+        cin >> node;
+ 
+        q = new Node;
+        q->data = node;
+        q->next = nullptr;
+ 
+        if (!head) {
+            head = last = q;
+        } else {
+            last->next = q;
+            last = q;
+        }
+    }
+}
+ 
+void split(Node *head, Node *&l1, Node *&l2) {
+    Node *evenHead = new Node; // for even
+    Node *oddHead = new Node;  // for odd
+ 
+    Node *evenLast = evenHead;
+    Node *oddLast = oddHead;
+ 
+    while (head != nullptr) {
+        if (head->data % 2 == 1) {
+            oddLast->next = head;
+            oddLast = head;
+            head = head->next;
+            oddLast->next = nullptr;
+        } else {
+            evenLast->next = head;
+            evenLast = head;
+            head = head->next;
+            evenLast->next = nullptr;
+        }
+    }
+ 
+    l1 = evenHead->next;
+    l2 = oddHead->next;
+ 
+    delete evenHead;
+    delete oddHead;
+}
+ 
+void display(Node *head) {
+    while (head) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+ 
 int main(int argc, char const *argv[]) {
-
-  return 0;
+    Node *head, *L1, *L2;
+    CreateList(head);
+    display(head);
+ 
+    split(head, L1, L2);
+ 
+    cout << "Even List: ";
+    display(L1);
+ 
+    cout << "Odd List: ";
+    display(L2);
+ 
+    return 0;
 }
