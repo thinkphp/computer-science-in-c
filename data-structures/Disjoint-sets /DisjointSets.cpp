952d02a41c4bbdf5a4b4a2e67e8a7d11ac980760
Adrian Statescu <mergesortv@gmail.com>
2023-12-19 09:15:47 +0200

    Create DisjointSets.cpp

---
+++ data-structures/Disjoint-sets /DisjointSets.cpp
@@ -0,0 +1,33 @@
+class DisjointSet {
+	vector<int> rank, parent;
+public:
+	DisjointSet(int n) {
+		rank.resize(n + 1, 0);
+		parent.resize(n + 1);
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+		}
+	}
+ 
+	int get(int node) {
+		if (node == parent[node])
+			return node;
+		return parent[node] = get(parent[node]);
+	}
+ 
+	void join(int u, int v) {
+		int ulp_u = get(u);
+		int ulp_v = get(v);
+		if (ulp_u == ulp_v) return;
+		if (rank[ulp_u] < rank[ulp_v]) {
+			parent[ulp_u] = ulp_v;
+		}
+		else if (rank[ulp_v] < rank[ulp_u]) {
+			parent[ulp_v] = ulp_u;
+		}
+		else {
+			parent[ulp_v] = ulp_u;
+			rank[ulp_u]++;
+		}
+	}
+};
