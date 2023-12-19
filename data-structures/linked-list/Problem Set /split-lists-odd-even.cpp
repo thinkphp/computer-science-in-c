14d1ad481476f1bdc0882a3017c68fe396d10e1a
Adrian Statescu <mergesortv@gmail.com>
2023-12-19 21:12:38 +0200

    Update split-lists-odd-even.cpp

--- data-structures/linked-list/Problem Set /split-lists-odd-even.cpp
+++ data-structures/linked-list/Problem Set /split-lists-odd-even.cpp
@@ -1,1 +1,47 @@
+/*
+                _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . ___
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+Book:
+Fundamentele Programarii. Culegere de probleme pentru clasa a X1-a. Autori: Dana Lica si Mircea Pasoi
 
+Consideram o lista simplu inlantuita ce memoreaza valori intregi.
+Realizati un subprogram care primeste la apel, prin intermediul unui
+unui parametru p, adresa de inceput a unei astfel de liste, separa
+elementele in doua liste, una formata din valori pare, iar cealalta, din valori impare.
+Subprogramul va returna adresele de inceput ale celor doua liste create prin intermediul a doi parametri.
+
+
+
+Input:
+
+Output:
+
+*/
+#include <iostream>
+using namespace std;
+struct Node {
+     int data;
+     struct Node *next;
+};
+
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
