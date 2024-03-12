34d7021309f498547fa1925f610939b7915aed38
Adrian Statescu <mergesortv@gmail.com>
2024-03-12 06:03:45 +0200

    Create subsets.cpp

---
+++ foundations/recursion/subsets.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define SIZE 100
+using namespace std;
+
+int sol[ SIZE ], n, v[SIZE];
+
+void read_vector() {
+
+    cout<<"n=";
+    cin>>n;
+    for(int i = 1; i <= n; ++i) cin>>v[i];
+}
+
+void gen_subsets(int k) {
+
+    if(k == 0) {
+
+       for(int i = 1; i<=n; ++i) cout<<v[i]<<" ";
+       cout<<endl;
+
+    } else {
+
+       for(int b = 1; b >= 0; --b) {
+         v[k] = b;
+         gen_subsets(k-1);
+       }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+  read_vector();
+  gen_subsets(n);
+  return 0;
+}
