57b0b51aae93d3052483f0a7a686c67efc8f0fd7
Adrian Statescu <mergesortv@gmail.com>
2025-08-09 08:52:07 +0300

    Update number_of_islands_leetcode.cpp

--- data-structures/graphs/BFS /number_of_islands_leetcode.cpp
+++ data-structures/graphs/BFS /number_of_islands_leetcode.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0, offsets[] = {0, 1, 0, -1, 0};
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    grid[i][j] = '0';
-                    queue<pair<int, int>> todo;
-                    todo.push({i, j});
-                    while (!todo.empty()) {
-                        pair<int, int> p = todo.front();
-                        todo.pop();
-                        for (int k = 0; k < 4; k++) {
-                            int r = p.first + offsets[k], c = p.second + offsets[k + 1];
-                            if (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] == '1') {
-                                grid[r][c] = '0';
-                                todo.push({r, c});
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int>drow = {-1,0,1,0};
+        vector<int>dcol = {0,1,0,-1};
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && grid[i][j]=='1'){
+                    ans++;
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        auto curr = q.front();
+                        q.pop();
+                        int x = curr.first;
+                        int y = curr.second;
+                        vis[x][y] = 1;
+                        for(int l=0;l<4;l++){
+                            int r =x+drow[l];
+                            int c =y+dcol[l];
+                            if(r>=0 && r<n && c>=0 && c<m && vis[r][c]==0 && grid[r][c]=='1'){
+                                q.push({r,c});
+                                vis[r][c]=1;
                             }
                         }
                     }
                 }
             }
         }
-        return islands;
+        return ans;
     }
 };
